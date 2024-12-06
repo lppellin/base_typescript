8e133bd61ebce433ec7e832141acfb29a36847c0
lppellin <lucaspaolopellin@gmail.com>
2024-12-05 21:54:01 -0300

    initialize project with TypeScript, ESLint, Prettier, and basic configuration files

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+    extends: [eslint.configs.recommended, tseslint.configs.recommended],
+    rules: {
+        'no-magic-numbers': 'off',
+        '@typescript-eslint/no-magic-numbers': 'error',
+    },
+});
